5e434d6b30841d426d9d8701f8b9a622843d6828
tyoprataama <tyoprataama004@gmail.com>
2023-01-25 20:49:28 +0700

    DOM manipulation js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const wrapper = document.querySelector('.slider-wrap');
 const menuItems = document.querySelectorAll('.menu-item');
-const products = [
-    {
+const products = [{
         id: 1,
         title: 'POLO ANAK',
         price: 49.000,
-        colors: [
-            {
+        colors: [{
                 code: 'green',
                 img: './img/polo.svg',
             },
@@ -19,7 +17,7 @@ const products = [
     {
         id: 2,
         title: 'KAOS ANAK',
-        price: 29.000,
+        price: 29,
         colors: [{
                 code: 'grey',
                 img: './img/kaos-anak.svg',
@@ -33,7 +31,7 @@ const products = [
     {
         id: 3,
         title: 'SETELAN ANAK',
-        price: 49.000,
+        price: 49,
         colors: [{
                 code: 'pink',
                 img: './img/setelan.svg',
@@ -47,13 +45,13 @@ const products = [
     {
         id: 4,
         title: 'TIE DYE ANAK',
-        price: 49.000,
+        price: 39,
         colors: [{
                 code: 'white',
                 img: './img/koleksi.svg',
             },
             {
-                code: 'blue',
+                code: 'lightblue',
                 img: './img/koleksi-biru.svg'
             }
         ]
@@ -61,13 +59,13 @@ const products = [
     {
         id: 5,
         title: 'AKSESORIS KUCING',
-        price: 49.000,
+        price: 19,
         colors: [{
                 code: 'grey',
                 img: './img/aksesoris.svg',
             },
             {
-                code: 'blue',
+                code: 'lightblue',
                 img: './img/aksesoris-biru.svg'
             }
         ]
@@ -84,5 +82,30 @@ menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         //change the current slide
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
+        //change the choosen product
+        choosenProduct = products[index];
+        //change texts of currentProduct
+        currentProductTitle.textContent = choosenProduct.title;
+        currentProductPrice.textContent = "Rp." + choosenProduct.price + ".000";
+        currentProductImg.src = choosenProduct.colors[0].img;
+        //assign new colors
+        currentProductColors.forEach((color, index) => {
+            color.style.backgroundColor = choosenProduct.colors[index].code;
+        });
+    });
+    currentProductColors.forEach((color, index) => {
+        color.addEventListener("click", () => {
+            currentProductImg.src = choosenProduct.colors[index].img;
+        });
+    });
+    currentProductSizes.forEach((size, index) => {
+        size.addEventListener("click", () => {
+            currentProductSizes.forEach((size) => {
+                size.style.backgroundColor = "white";
+                size.style.color = "black";
+            });
+            size.style.backgroundColor = "black";
+            size.style.color = "white";
+        });
     });
 });
